b90abbdbf02ed3dd94cc52acfaf2b75adb349ec2
ant05yt <ant05.dambrosio@gmail.com>
2025-11-22 16:57:59 +0100

    👷 setup for work

---
+++ FitHub/src/test/java/it/fithub/fithubspring/FitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package it.fithub.fithubspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
