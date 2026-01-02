8c9ea59fda91a9b46a962b1a47c3c3acd2ea7865
Alessandro-CC05 <aleandroc05@gmail.com>
2026-01-02 13:13:20 +0100

    .

--- FitHub-ng/src/app/app.routes.ts
+++ FitHub-ng/src/app/app.routes.ts
@@ -4,12 +4,17 @@ import { MapFinderComponent } from './map-finder/map-finder.component';
 export const routes: Routes = [
   {
     path: '',
+    loadComponent: () => import('./landing-page/landing-page.component').then(m => m.LandingPageComponent),
+    title: 'FitHub - Your Fitness Journey'
+  },
+  {
+    path: 'map',
     component: MapFinderComponent,
     title: 'FitHub - Find Your Spot'
   },
   {
     path: 'map-finder',
-    redirectTo: '',
+    redirectTo: 'map',
     pathMatch: 'full'
   }
 ];
---
+++ FitHub-ng/src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-landing-page',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.css']
+})
+export class LandingPageComponent {
+  // Mobile menu state
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+}
